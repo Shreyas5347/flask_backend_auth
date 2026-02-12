225e70ed9ed51477cba110ebb12f906ee0a16e71
Shreyas5347 <rajputshreyas321@gmail.com>
2026-02-12 22:30:20 +0530

    used postman

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from auth import auth_bp
 
 
 app = Flask(__name__)
 app.register_blueprint(auth_bp)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Flask Backend Auth API", "status": "running"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
